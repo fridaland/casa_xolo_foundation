b4246cdb3ee8575089bcd443e10ed9a7cc55891e
Frida Casas <frida@thoughtbot.com>
2025-12-11 10:27:44 -0800

    Configure rspec

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module CasaXoloFoundation
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.generators.test_framework = :rspec
 
     # Configuration for the application, engines, and railties goes here.
     #
