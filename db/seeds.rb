b3b0928567c04fa0d861d0555958b6698a619cd6
Frida Casas <frida@thoughtbot.com>
2025-12-11 11:07:06 -0800

    add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "factory_bot_rails"
+
+FactoryBot.create(:user)
---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    email { "tod@thefox.com" }
+    password { "password123" }
+    name { "Tod Fox" }
+  end
+end
