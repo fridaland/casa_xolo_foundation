e0896867a362adb74f9d2191508ab077054ffd98
Sally Hall <sally@thoughtbot.com>
2025-12-12 15:21:33 -0500

    maybe click link?

--- spec/system/admin/pets_spec.rb
+++ spec/system/admin/pets_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Admin::Pets", type: :system do
     login_as user
     visit "/admin/pets"
     expect(page).to have_content("New pet")
-    click_on "New pet"
+    click_link "New pet"
 
     fill_in "Name", with: "Tater Tot"
     select "dog", from: "Species"
