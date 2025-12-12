86af7012e611974ef082c72fb6f123396ed82f1b
Sally Hall <sally@thoughtbot.com>
2025-12-12 15:51:12 -0500

    Add Procfile to run migrations
    In order to automatically run migrations on heroku, we need
    a Procfile. This also adds a migration so we can ensure the
    Procfile is working.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_12_190537) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_12_205055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_12_190537) do
 
   create_table "people", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
