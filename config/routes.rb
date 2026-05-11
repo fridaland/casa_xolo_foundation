b2aea86e4007b19817c1fafa9075d2617a3d747f
Frida Casas <frida@thoughtbot.com>
2026-05-11 16:25:28 -0700

    Volunteering page
    * Adds a new page for volunteering opportunities, including a hero section, volunteer opportunities,and a list of requirements for volunteers.

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
   get "/" => "home#index", :as => :home
+  get "/volunteer" => "pages#volunteer", :as => :volunteer
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
