1d0146269071f923eda12cc2d04328b1cc1bb1d3
Frida Casas <frida@thoughtbot.com>
2026-06-05 16:18:21 -0700

    lint

--- app/models/volunteer_application.rb
+++ app/models/volunteer_application.rb
@@ -10,13 +10,13 @@ class VolunteerApplication < ApplicationRecord
     "All of the above, where needed"
   ].freeze
 
-  PHONE_REGEXP = /\A(\+?1[\s.\-]?)?\(?\d{3}\)?[\s.\-]?\d{3}[\s.\-]?\d{4}\z/
+  PHONE_REGEXP = /\A(\+?1[\s.-]?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/
   EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, :email, :phone, :position, :message, presence: true
-  validates :email, format: { with: EMAIL_REGEXP }
-  validates :phone, format: { with: PHONE_REGEXP }, allow_blank: true
-  validates :position, inclusion: { in: POSITIONS }, allow_blank: true
+  validates :email, format: {with: EMAIL_REGEXP}
+  validates :phone, format: {with: PHONE_REGEXP}, allow_blank: true
+  validates :position, inclusion: {in: POSITIONS}, allow_blank: true
 
   before_create { self.submitted_at = Time.current }
 end
--- spec/factories/volunteer_applications.rb
+++ spec/factories/volunteer_applications.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :volunteer_application do
-    name     { Faker::Name.name }
-    email    { Faker::Internet.email }
-    phone    { Faker::PhoneNumber.phone_number }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    phone { Faker::PhoneNumber.phone_number }
     position { VolunteerApplication::POSITIONS.sample }
-    message  { Faker::Lorem.paragraph(sentence_count: 2) }
+    message { Faker::Lorem.paragraph(sentence_count: 2) }
   end
 end
