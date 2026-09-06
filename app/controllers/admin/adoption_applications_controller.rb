56aa63c6df373b44ab6edfda5faf3017af68a2e8
Frida Casas <frida@thoughtbot.com>
2026-09-06 15:51:14 -0700

    prefer scope on model (#159)

--- app/controllers/admin/adoption_applications_controller.rb
+++ app/controllers/admin/adoption_applications_controller.rb
@@ -1,10 +1,7 @@
 module Admin
   class AdoptionApplicationsController < Admin::ApplicationController
     def scoped_resource
-      super.order(
-        Arel.sql("CASE status WHEN 'pending' THEN 0 WHEN 'reviewing' THEN 1 WHEN 'approved' THEN 2 ELSE 3 END"),
-        submitted_at: :desc
-      )
+      super.by_status_priority
     end
   end
 end
